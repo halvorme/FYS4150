c4a3666e69993678d2e0f6f315268bf6ff151e25
halvorme <halvorme@proton.me>
2022-09-14 00:38:35 +0200

    Untracked testing folder

--- testing/header.hpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-
-void print_message(){
-	std::cout << "Hello from the leg" << std::endl;
-}
-
--- testing/headtest.cpp
+++
@@ -1,6 +0,0 @@
-#include "header.hpp"
-
-int main(){
-	print_message();
-	return 0;
-}
